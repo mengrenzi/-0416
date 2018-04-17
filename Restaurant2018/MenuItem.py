36d9b9033c7ef578be0f19227959e11334f1c5a6
lazzzis <shenlijin@outlook.com>
2018-04-17 19:26:37 +0000

    implement menu and menuitem; update gitignore

--- Restaurant2018/MenuItem.py
+++ Restaurant2018/MenuItem.py
@@ -8,6 +8,40 @@ class MenuItem(object):
         self.__price = price
         self.__description = description
 
+    # more format tricks: https://pyformat.info/
     def __str__(self):
-        message = self.__name + '(' + self.__type + '):' + '$' + self.__price + "\n" + self.__description
-        return message
+        return '{p.name} ({p.type}): ${p.price}\n{p.description}'.format(p=self)
+
+    __repr__ = __str__
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @property
+    def type(self):
+        return self.__type
+
+    @type.setter
+    def type(self, value):
+        self.__type = value
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        self.__price = value
+
+    @property
+    def description(self):
+        return self.__description
+
+    @description.setter
+    def description(self, value):
+        self.__description = value
